038ae4bdaec4b35224c30cee155813c7b4040f3b
MukhailoK <96546140+MukhailoK@users.noreply.github.com>
2022-12-22 18:50:40 +0100

    back pattern State

--- src/main/java/com/softserve/itacademy/model/User.java
+++ src/main/java/com/softserve/itacademy/model/User.java
@@ -4,6 +4,9 @@ import lombok.*;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.util.List;
 
@@ -18,13 +21,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false, unique = true)
     private Long id;
+    @Email
+    @NotBlank
     @Column(name = "email", nullable = false, unique = true)
     private String email;
     @Column(name = "first_name", nullable = false)
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z\\d\\-\\s_]{1,20}$", message = "The stateName must contain from 1 to 20 latin letters, numbers, dash, space and underscore")
     private String firstName;
     @Column(name = "last_name", nullable = false)
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z\\d\\-\\s_]{1,20}$", message = "The stateName must contain from 1 to 20 latin letters, numbers, dash, space and underscore")
     private String lastName;
     @Column(name = "password", nullable = false)
+    @NotNull
     private String password;
     @ManyToOne
     @JoinColumn(name = "role_id")
