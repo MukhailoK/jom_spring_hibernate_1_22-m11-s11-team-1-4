d7ba1b8fd254991851f2f4c2359874951a6a9c1a
MukhailoK <96546140+MukhailoK@users.noreply.github.com>
2022-12-22 15:31:04 +0100

    add docker yaml

--- src/main/java/com/softserve/itacademy/model/State.java
+++ src/main/java/com/softserve/itacademy/model/State.java
@@ -1,5 +1,41 @@
 package com.softserve.itacademy.model;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "states")
 public class State {
- //TODO
+
+    @Setter(AccessLevel.PRIVATE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Pattern(regexp = "^[a-zA-Z\\d\\-\\s_]{1,20}$", message = "The stateName must contain from 1 to 20 latin letters, numbers, dash, space and underscore")
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        State state = (State) o;
+        return id != null && Objects.equals(id, state.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
