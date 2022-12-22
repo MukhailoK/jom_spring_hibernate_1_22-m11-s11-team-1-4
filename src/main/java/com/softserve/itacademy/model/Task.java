a4f7c262d880dd5bf7e70442fdb91fef40345014
Sehii <dolivo.serg@gmail.com>
2022-12-22 11:57:00 +0200

    class_Task

--- src/main/java/com/softserve/itacademy/model/Task.java
+++ src/main/java/com/softserve/itacademy/model/Task.java
@@ -3,8 +3,6 @@ package com.softserve.itacademy.model;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
